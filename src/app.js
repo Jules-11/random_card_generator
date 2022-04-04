d22290eb06950b83105a5d9ba1d932a4309a4902
Jules-11 <julien.galeuzzi@gmail.com>
2022-04-04 08:53:48 +0000

    Random Card Generator Project

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let dealNewCard = () => {
+  let suit = Math.floor(Math.random() * 4);
+  let number = Math.floor(Math.random() * 12);
+  let arrSuit = [
+    "bi-suit-spade-fill",
+    "bi-suit-club-fill",
+    "bi-suit-heart-fill",
+    "bi-suit-diamond-fill"
+  ];
+  let arrNumber = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let topSuit = document.getElementById("topSuit");
+  let cardNumber = document.getElementById("cardNumber");
+  let bottomSuit = document.getElementById("bottomSuit");
+
+  cardNumber.innerHTML = arrNumber[number];
+  topSuit.removeAttribute("class");
+  bottomSuit.removeAttribute("class");
+  topSuit.classList.add("bi");
+  bottomSuit.classList.add("bi");
+  topSuit.classList.add(arrSuit[suit]);
+  bottomSuit.classList.add(arrSuit[suit]);
+
+  if (
+    arrSuit[suit] == "bi-suit-spade-fill" ||
+    arrSuit[suit] == "bi-suit-club-fill"
+  ) {
+    topSuit.style.color = "black";
+    bottomSuit.style.color = "black";
+    cardNumber.style.color = "black";
+  } else {
+    topSuit.style.color = "red";
+    bottomSuit.style.color = "red";
+    cardNumber.style.color = "red";
+  }
+};
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  dealNewCard();
+
+  let buttonClick = document.getElementById("btn");
+  buttonClick.addEventListener("click", dealNewCard);
 };
